cdc84419c29218561e710cdb9672d9003106fb1a
nopperl <54780682+nopperl@users.noreply.github.com>
2024-01-09 23:08:12 +0100

    Automatically redirect requests to the www.reddit.com/media HTML page to the contained image (fixes #6).

--- background.js
+++ background.js
@@ -1,3 +1,13 @@
+function redirect(details) {
+  const url = new URL(details.url);
+  const targetURL = url.searchParams.get("url");
+  if (targetURL) {
+    return {
+      redirectUrl: targetURL
+    };
+  }
+}
+
 function modifyAcceptHeader(details) {
   let newHeaders = details.requestHeaders.map(header => {
     if (header.name.toLowerCase() === 'accept') {
@@ -12,9 +22,13 @@ function modifyAcceptHeader(details) {
   return { requestHeaders: newHeaders };
 }
 
+browser.webRequest.onBeforeRequest.addListener(
+  redirect,
+  { urls: ["*://www.reddit.com/media*"], types: ["main_frame"] },
+  ["blocking"]
+);
 browser.webRequest.onBeforeSendHeaders.addListener(
   modifyAcceptHeader,
   { urls: ['*://i.redd.it/*', '*://external-preview.redd.it/*', '*://preview.redd.it/*'] },
   ['blocking', 'requestHeaders']
 );
-
