b977ec69f14d98eb1c0183ec9cc3028c70ef55a1
nopperl <54780682+nopperl@users.noreply.github.com>
2024-03-13 19:18:45 +0100

    fix poor performance of lightbox replacement, and enable lightbox replacement only on post pages (fixes #11)

--- content.js
+++ content.js
@@ -1,11 +1,19 @@
 const pollingInterval = 500;
 
 function replaceLightboxElementWithAnchor() {
+  // only disable lightbox for /r/*/comments/* pages
+  if (!(window.location.host === "www.reddit.com" && window.location.pathname.includes("/comments/"))) {
+    return;
+  }
   browser.storage.local.get('disableLightbox', function(result) {
     if (result.disableLightbox) {
       const images = document.querySelectorAll('img.media-lightbox-img');
+      console.log(images);
       for (let i = 0; i < images.length; i++) {
         const img = images[i];
+	if (img.parentNode.nodeName == "A") {
+	  continue;
+	}
         const a = document.createElement('a');
         a.setAttribute('href', img.getAttribute('src'));
         img.parentNode.insertBefore(a, img);
@@ -15,6 +23,6 @@ function replaceLightboxElementWithAnchor() {
   });
 }
 
-// Schedule the initial check and start polling
 replaceLightboxElementWithAnchor();
+// regularly attempt to replace the lightbox as the window.location might have changed to a post page using history.pushState
 setInterval(replaceLightboxElementWithAnchor, pollingInterval);
